8e0264d6cf23d0203a39861fb291066c75b4fd20
ahmadrafiq <135581522+ahmadrafiq-wego@users.noreply.github.com>
2024-07-16 14:33:56 +0500

    first commit of project setup

---
+++ LiveScoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveScoreApp
+//
+//  Created by Ahmad Rafiq on 03/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
