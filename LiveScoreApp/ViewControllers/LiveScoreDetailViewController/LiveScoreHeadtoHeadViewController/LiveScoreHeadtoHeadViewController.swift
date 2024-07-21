6bf632c836c32ce4566a6475c76668fc4a43e023
ahmadrafiq <135581522+ahmadrafiq-wego@users.noreply.github.com>
2024-07-21 14:55:08 +0500

    chore: UI of liveScoreHeadToHead screen implement

--- LiveScoreApp/ViewControllers/LiveScoreDetailViewController/LiveScoreHeadtoHeadViewController/LiveScoreHeadtoHeadViewController.swift
+++ LiveScoreApp/ViewControllers/LiveScoreDetailViewController/LiveScoreHeadtoHeadViewController/LiveScoreHeadtoHeadViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class LiveScoreHeadtoHeadViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UINib(nibName: LiveScoreHeadToHeadTableViewCell.className, bundle: nil), forCellReuseIdentifier: LiveScoreHeadToHeadTableViewCell.className)
+    }
 
-        // Do any additional setup after loading the view.
+
+}
+
+extension LiveScoreHeadtoHeadViewController : UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = LiveScoreHeadToHeadHeaderView.view()
+        return view
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: LiveScoreHeadToHeadTableViewCell.className, for: indexPath) as! LiveScoreHeadToHeadTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.removeSelection()
+    }
 }
---
+++ LiveScoreApp/ViewControllers/LiveScoreDetailViewController/LiveScoreHeadtoHeadViewController/cells/LiveScoreHeadToHeadHeaderView.swift
@@ -0,0 +1,22 @@
+//
+//  LiveScoreHeadToHeadHeaderView.swift
+//  LiveScoreApp
+//
+//  Created by Ahmad Rafiq on 21/07/2024.
+//
+
+import UIKit
+
+class LiveScoreHeadToHeadHeaderView: UIView {
+
+    static func view() -> LiveScoreHeadToHeadHeaderView {
+        let v = Bundle.main.loadNibNamed(LiveScoreHeadToHeadHeaderView.className, owner: nil, options: nil)?.first as! LiveScoreHeadToHeadHeaderView
+        v.setup()
+        return v
+    }
+    
+    func setup() {
+        
+    }
+
+}
