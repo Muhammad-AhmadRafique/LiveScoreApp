7ce5b342528636020fa70167443fc418bb362141
ahmadrafiq <135581522+ahmadrafiq-wego@users.noreply.github.com>
2024-08-04 20:25:29 +0500

    news screen UI update and navigation to LiveScore Detail screen

--- LiveScoreApp/ViewControllers/NewsViewController/Cells/NewsHotMatchesTableViewCell.swift
+++ LiveScoreApp/ViewControllers/NewsViewController/Cells/NewsHotMatchesTableViewCell.swift
@@ -10,8 +10,11 @@ import UIKit
 class NewsHotMatchesTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collectionView: UICollectionView!
-    var liveScoreModelList: [LiveScoreModel]?
+    @IBOutlet weak var noDataLabel: UILabel!
+    var liveScoreModelList: [LiveScoreModel] = []
     
+    var matchTapped: ((LiveScoreModel) -> ())?
+        
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -25,32 +28,30 @@ class NewsHotMatchesTableViewCell: UITableViewCell, UICollectionViewDelegate, UI
         // Configure the view for the selected state
     }
     
-    func configureCell(liveScoreModelList: [LiveScoreModel]?) {
-        let totalCount = liveScoreModelList?.count ?? 0
-        if totalCount > 5 {
-            for i in 0..<5 {
-                if let model = liveScoreModelList?[i] {
-                    self.liveScoreModelList?.append(model)
-                }
-            }
-        } else {
-            self.liveScoreModelList = liveScoreModelList
-        }
+    func configureCell(liveScoreModelList: [LiveScoreModel]) {
+        self.liveScoreModelList = liveScoreModelList
+        self.noDataLabel.isHidden = !liveScoreModelList.isEmpty
         self.collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return liveScoreModelList?.count ?? 0
+        return liveScoreModelList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewsHotMatchesCollectionViewCell.className, for: indexPath) as! NewsHotMatchesCollectionViewCell
-        cell.configureCell(model: liveScoreModelList?[indexPath.row])
+        cell.configureCell(model: liveScoreModelList[indexPath.item])
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = CGSize(width: 200, height: collectionView.frame.height)
+        let width = (collectionView.frame.width / 2) - 10
+        let size = CGSize(width: width, height: 202)
         return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = liveScoreModelList[indexPath.item]
+        matchTapped?(item)
     }
 }
---
+++ LiveScoreApp/ViewControllers/NewsViewController/NewsUpdatedViewController.swift
@@ -0,0 +1,116 @@
+//
+//  NewsUpdatedViewController.swift
+//  LiveScoreApp
+//
+//  Created by Ahmad Rafiq on 04/08/2024.
+//
+
+import UIKit
+
+class NewsUpdatedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    var liveScoreLeagueList = [LiveScoreModel]()
+
+    private var maxHotMatchesCount = 5
+
+    //MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UINib(nibName: NewsHotMatchesTableViewCell.className, bundle: nil), forCellReuseIdentifier: NewsHotMatchesTableViewCell.className)
+        tableView.tableHeaderView = NewsTableHeaderView.view()
+        tableView.contentInsetAdjustmentBehavior = .never
+        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        getUpcomingMatches()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+}
+
+extension NewsUpdatedViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = NewsHeaderView.view()
+        view.configure(title: "Hot Matches", font: .systemFont(ofSize: 20, weight: .bold))
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewsHotMatchesTableViewCell.className, for: indexPath) as! NewsHotMatchesTableViewCell
+        cell.configureCell(liveScoreModelList: liveScoreLeagueList)
+        cell.matchTapped = { match in
+            Router.shared.openLiveScoreDetailViewController(model: match, controller: self.navigationController)
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let perRowItems = liveScoreLeagueList.count / 2
+        return CGFloat(202 * perRowItems)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.removeSelection()
+    }
+}
+
+extension NewsUpdatedViewController {
+    
+    @objc private func getUpcomingMatches() {
+        showProgressHud()
+        
+        let url = API.Leagues.Football.liveScore
+        APIGeneric<LiveScoreResponseModel>.fetchRequest(apiURL: url) { [weak self] (response) in
+            guard let `self`  = self else { return }
+            DispatchQueue.main.async {
+                self.hideProgressHud()
+                switch response {
+                case .success(let result):
+                    let success = ResponseType(rawValue: result.success ?? ResponseType.error.rawValue)
+                    switch success {
+                    case .success:
+                        var list = (result.result ?? []).filter({$0.eventStatus?.lowercased() != "finished"})
+                        list = Array(list.prefix(self.maxHotMatchesCount))
+                        let updatedList = Helper.groupLiveScoreMatchesByLeagues(list: list)
+                        updatedList.forEach { model in
+                            if let firstMatch = model.matchList?.first {
+                                self.liveScoreLeagueList.append(firstMatch)
+                            }
+                        }
+                        self.tableView.reloadData()
+                    default:
+                        let err = CustomError(description: "Something went wrong, please try again")
+                        self.alertMessage(title: K.ERROR, alertMessage: err.description ?? "", action: nil)
+                    }
+                case .failure(let failure):
+                    let err = CustomError(description: (failure as? CustomError)?.description ?? "")
+                    self.alertMessage(title: K.ERROR, alertMessage: err.description ?? "", action: nil)
+                }
+            }
+        }
+    }
+}
